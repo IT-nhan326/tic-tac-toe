b8a65b67115fae0f09da791dbeb3001f34aa361e
newgate.p <newgate.p@itcgroup.io>
2023-08-21 12:14:38 +0700

    feat/scss - add scss and apply scss for components

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="board">
+    <div class="row" v-for="rowIndex in 3" :key="rowIndex">
+      <div class="block" v-for="colIndex in 3" :key="colIndex"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.board {
+  width: 9rem; /* Width of the entire board */
+  height: 9rem; /* Height of the entire board */
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem; /* Gap between blocks */
+  border: 1px solid #000;
+  padding: 1rem;
+}
+
+.row {
+  display: flex;
+  justify-content: space-between;
+}
+
+.block {
+  width: 2rem; /* Width of each block */
+  height: 2rem; /* Height of each block */
+  background-color: #eee;
+  border: 1px solid #ccc;
+}
+</style>
---
+++ src/components/StartGameButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="start-game">
+    <transition name="fade">
+      <button class="start-button" @click="startGame">Start Game</button>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StartGame",
+  methods: {
+    startGame() {
+      // Navigate to the new game route
+      this.$router.push("/start-game");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/_variables";
+@import "@/assets/styles/_mixins";
+
+.start-game {
+  @include center;
+  height: 100vh;
+}
+
+.start-button {
+  background-color: $primary-color;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  @include transition(background-color);
+
+  &:hover {
+    background-color: $secondary-color;
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  @include transition(opacity);
+}
+
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0;
+}
+</style>
